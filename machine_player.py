90864703722ac1fb1af6ab5d8795d3286cd893da
David Duenn <davidduenn@gmail.com>
2018-06-08 11:46:48 -0400

    Add simple machine player

---
+++ machine_player.py
@@ -0,0 +1,9 @@
+def generate_simple_move(board):
+  # Run through the lists and return the
+  # first discovered blank spot as the move
+  for row, x in enumerate(board):
+    try:
+      column = x.index(0)
+    except ValueError:
+      continue
+    return row,column
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from winning import get_winner, print_winner
 from helpers import get_input, valid_move, make_move
 from printing import print_board
+from machine_player import generate_simple_move
 
-
+player_type = 1
+  # 0: human, 1: simple machinge, 2: difficult machine
   
 def main():
   board = [[0,0,0],[0,0,0],[0,0,0]]
   x_turn = 1
   while 1:
     print_board(board)
-    [x,y] = get_input(x_turn)
-    while not valid_move(board, x, y):
+    if player_type and not x_turn:
+      # machine player will play as O
+      [x,y] = generate_simple_move(board)
+    else:
       [x,y] = get_input(x_turn)
+      while not valid_move(board, x, y):
+        [x,y] = get_input(x_turn)
     if x_turn:
       make_move(board, x, y, 1)
       x_turn=0
@@ -23,6 +29,5 @@ def main():
     if winner:
       print_winner(winner)
       return 0
-  
 
 main()
