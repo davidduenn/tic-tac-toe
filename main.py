9aa1d059a5052403794337d9b43a7ccc383f30f7
David Duenn <davidduenn@gmail.com>
2018-09-16 20:51:25 -0400

    main.py: break o-player off, store moves and push to db at end of game

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import redis
 
+from copy import deepcopy # for copying the board to *_moves
+
 from winning import get_winner, print_winner
 from helpers import get_input, valid_move, make_move
 from printing import print_board
@@ -11,7 +13,12 @@ def main():
   x_player_type = 3 # 0: human, 1: simple machine, 2: random machine, 3: difficult machine
   o_player_type = 2 # 0: human, 1: simple machine, 2: random machine, 3: difficult machine
   board = [[0,0,0],[0,0,0],[0,0,0]]
+  x_moves=[]
+  o_moves=[]
+  r = redis.Redis(host="localhost")
   x_turn = 1 # X goes first
+
+  from helpers import clean_move_for_db
 
   # Game loop
   while 1:
@@ -30,9 +37,10 @@ def main():
         while not valid_move(board, x, y):
           [x,y] = generate_random_move(board)
       if x_player_type==3:
-        [x,y] = generate_complex_move(board)
+        [x,y] = generate_complex_move(board, r)
         while not valid_move(board, x, y):
-          [x,y] = generate_complex_move(board)
+          [x,y] = generate_complex_move(board, r)
+      x_moves.append([deepcopy(board), 1, x, y])
       make_move(board, x, y, 1)
       x_turn=0
     else: # O's turn
@@ -52,13 +60,24 @@ def main():
         [x,y] = generate_complex_move(board, r)
         while not valid_move(board, x, y):
           [x,y] = generate_complex_move(board, r)
+      o_moves.append([deepcopy(board), 2, x, y])
       make_move(board, x, y, 2)
       x_turn=1
 
-    # If winner: print, exit
+    # If winner: print, record moves, exit
     winner = get_winner(board)
     if winner:
       print_winner(winner)
+      if winner is 1:
+        for move in x_moves:
+          r.incr(clean_move_for_db(move))
+        for move in o_moves:
+          r.decr(clean_move_for_db(move))
+      else:
+        for move in x_moves:
+          r.decr(clean_move_for_db(move))
+        for move in o_moves:
+          r.incr(clean_move_for_db(move))
       return 0
 
 main()
